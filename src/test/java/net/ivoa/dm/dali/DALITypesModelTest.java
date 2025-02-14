0e57571a3ef9519d24551e8442cb8b4ce20ea18c
Paul Harrison <paul.harrison@manchester.ac.uk>
2025-02-14 14:52:05 +0000

    add in a model to test use the dali types.

--- src/test/java/net/ivoa/dm/dali/DALITypesModelTest.java
+++ src/test/java/net/ivoa/dm/dali/DALITypesModelTest.java
@@ -3,23 +3,51 @@ package net.ivoa.dm.dali;
  * Created on 10/05/2023 by Paul Harrison (paul.harrison@manchester.ac.uk).
  */
 
+import net.ivoa.dm.dali.test.ATest;
+import net.ivoa.dm.dali.test.DalitestModel;
+import org.ivoa.vodml.testing.AutoDBRoundTripTest;
 import org.ivoa.vodml.testing.AutoRoundTripWithValidationTest;
+
+import java.util.List;
 
 /**
  * This will run a XML and JSON round trip test on the model inst.
  */
-public class DALITypesModelTest extends AutoRoundTripWithValidationTest<DaliModel> {
-    @Override
-    public DaliModel createModel() {
-        // create the model instance here.
-        DaliModel retval = new DaliModel();
+public class DALITypesModelTest extends AutoDBRoundTripTest<DalitestModel, Long, ATest> {
 
-//TODO really need to create another model that imports and does things with DALI one to do useful tests
+   private ATest aTest;
+
+   @Override
+    public DalitestModel createModel() {
+        // create the model instance here.
+        DalitestModel retval = new DalitestModel();
+      aTest = ATest.createATest(
+            t -> {
+               t.acircle = new Circle(new Point(1.1, 2.4), 4.0);
+               t.aninterval = new Interval(4.5, 7.1);
+               t.apoint = new Point(5.0, 6.0);
+               t.apoly = new Polygon(new Point[]{new Point(5.0, 6.0), new Point(5.0, 7.0), new Point(6.0, 8.0)});
+               t.amultishape = new MultiShape(List.of(
+                     new Circle(new Point(1.1, 2.4), 4.0), new Circle(new Point(2.4, 4.4), 2.1)
+               ));
+            }
+      );
+      retval.addContent(aTest);
         return retval;
     }
 
     @Override
-    public void testModel(DaliModel daliModel) {
+    public void testModel(DalitestModel daliModel) {
         //this could do specialized testing on the model instance
     }
+
+   @Override
+   public ATest entityForDb() {
+      return aTest;
+   }
+
+   @Override
+   public void testEntity(ATest aTest) {
+
+   }
 }
